ba3076f8ea4b1a8daf9b46200ff3804066b064b8
bcquant1 <47398183+bcquant1@users.noreply.github.com>
2019-08-03 14:39:12 -0700

    Create random.cpp

---
+++ Lab 2/Random/random.cpp
@@ -0,0 +1,42 @@
+//
+//  random.cpp
+//  Lab2Part2
+//
+//  Created by Justin Liu on 1/17/19.
+//  Copyright Â© 2019 SCU. All rights reserved.
+//
+
+#include <stdio.h>
+#include "random.h"
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+namespace coen79_lab2 {
+
+    rand_gen::rand_gen(int newseed, int newmultiplier, int newincrement, int newmodulus){
+        seed = newseed;
+        multiplier = newmultiplier;
+        increment = newincrement;
+        modulus = newmodulus;
+    }
+    
+    void rand_gen::set_seed(int new_seed){
+        seed = new_seed;
+    }
+
+    int rand_gen::next(){
+        int product = (multiplier * seed + increment) % modulus;
+        set_seed(product);
+        return product;
+    }
+
+    void rand_gen::print_info(){
+        cout << "Seed: " << seed <<endl;
+        cout << "Multiplier: " << multiplier <<endl;
+        cout << "Increment: " << increment <<endl;
+        cout << "Modulus: " << modulus <<endl;
+    }
+
+}
